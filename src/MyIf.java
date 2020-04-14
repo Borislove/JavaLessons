0c236fbb2aef9ff88aa82ae9b48420e338910785
Boris <abirme@yandex.ru>
2020-04-14 14:31:01 +0300

    Урок №35. Управляющий оператор if, else

---
+++ src/MyIf.java
@@ -0,0 +1,23 @@
+//Урок №35. Управляющий оператор if, else
+public class MyIf {
+
+    public static void main(String[] args) {
+
+        int a1 = 5;
+        int a2 = 1;
+
+        int res;
+
+        if (a2 != 0) {
+            res = a1 / a2;
+            if (a1 == 5) {
+                System.out.println("a1 = 5");
+            } else {
+                System.out.println("a1!=5");
+            }
+            System.out.println("Result is " + res);
+        } else {
+            System.out.println("Error, a2 = 0");
+        }
+    }
+}
