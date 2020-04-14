d4f7a722e27f1671c13f06e7a77af7beb8b2f5ed
Boris <abirme@yandex.ru>
2020-04-14 14:55:03 +0300

    Урок №36. Оператор цикла while, do-while

---
+++ src/MyDoWhile.java
@@ -0,0 +1,16 @@
+public class MyDoWhile {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 10);
+        System.out.println("Finish");
+    }
+}
+
+
---
+++ src/MyWhile.java
@@ -0,0 +1,14 @@
+//Урок №36. Оператор цикла while, do-while
+public class MyWhile {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish");
+    }
+}
