269241d8a41048d363ff019b652b638944065037
Boris <abirme@yandex.ru>
2020-04-18 21:20:41 +0300

    Урок №57. Java. Возврат объектов

---
+++ src/MyRet.java
@@ -0,0 +1,24 @@
+//Урок №57. Java. Возврат объектов
+public class MyRet {
+    int a;
+    int b;
+
+    MyRet(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    MyRet myDouble() {
+        MyRet ret = new MyRet(a * 2, b * 2);
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        MyRet r1 = new MyRet(1, 2);
+        MyRet r2;
+
+        r2 = r1.myDouble();
+        System.out.println("a = " + r1.a + " b = "+r1.b);
+        System.out.println("a = " + r2.a + " b = "+r2.b);
+    }
+}
