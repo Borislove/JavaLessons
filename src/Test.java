0d1e1b6cc29113614c131ba9d36b65d45a353d70
Boris <abirme@yandex.ru>
2020-04-18 20:33:17 +0300

    Урок №54. Ключевое слово this

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+//Урок №54. Ключевое слово this
+public class Test {
+    int a;
+    int b;
+
+    void setAB(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println(" a = " + a + " b = " + b);
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.setAB(2, 3);
+
+        System.out.println(" a = " + t.a + " b = " + t.b);
+    }
+}
