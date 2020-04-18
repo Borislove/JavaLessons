3ab91dd20cd30a6234d9a8196e1bb84da17edff3
Boris <abirme@yandex.ru>
2020-04-18 19:07:42 +0300

    Урок №48. Java. Пример работы с классом

---
+++ src/MyClass.java
@@ -0,0 +1,19 @@
+//Урок №48. Java. Пример работы с классом
+public class MyClass {
+    int myClass_properties;
+
+    void setMyClass_properties(int number) {
+        myClass_properties = number;
+    }
+
+    int getMyClass_properties() {
+        return myClass_properties;
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass;
+        myClass = new MyClass();
+        myClass.setMyClass_properties(5);
+        System.out.println("Result " + myClass.getMyClass_properties());
+    }
+}
