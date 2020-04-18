2278913d35b4438a194cea69afa8fa48be0d748c
Boris <abirme@yandex.ru>
2020-04-18 19:52:43 +0300

    Урок №49. Java. Переменные экземпляров

---
+++ src/Rectangle.java
@@ -0,0 +1,21 @@
+//Урок №49. Java. Переменные экземпляров
+public class Rectangle {
+    double width;
+    double height;
+
+    public static void main(String[] args) {
+
+        Rectangle rect, rect2;
+        rect = new Rectangle();
+        rect2 = new Rectangle();
+
+        rect.width = 10;
+        rect.height = 2;
+
+        rect2.width = 5;
+        rect2.height = 3;
+
+        System.out.println("Square 1 = " + (rect.width * rect.height));
+        System.out.println("Square 2 = " + (rect2.width * rect2.height));
+    }
+}
