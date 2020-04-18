b47de489444ce1dc941dee90527e02fe81961893
Boris <abirme@yandex.ru>
2020-04-18 18:27:09 +0300

    Урок №44 Двумерный массив

---
+++ src/MyDays.java
@@ -0,0 +1,13 @@
+//Урок №44
+public class MyDays {
+    public static void main(String[] args) {
+
+        int i = 11;
+        String days[][] = {{"31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"},
+                {
+                        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+                }};
+
+        System.out.println("Result -> "+days[1][i] + " has " + days[0][i] + " days.");
+    }
+}
