00cf7b78bc15ce4085ee477be0ad9a3eb97c09ec
Boris <abirme@yandex.ru>
2020-04-18 20:21:23 +0300

    Урок №53.Конструкторы

---
+++ src/MyRec.java
@@ -0,0 +1,17 @@
+//Урок №53. Java. Конструкторы
+public class MyRec {
+    double width;
+    double height;
+
+    public MyRec(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    public static void main(String[] args) {
+        MyRec rec = new MyRec(10,2);
+        MyRec rec2 = new MyRec(5,3);
+        System.out.println("Width = " + rec.width + " Height = " + rec.height);
+        System.out.println("Width 2 = " + rec2.width + " Height 2 = " + rec2.height);
+    }
+}
