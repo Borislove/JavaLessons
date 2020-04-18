a8fe26946247dfdfb989576b80e695fb4d3ba124
Boris <abirme@yandex.ru>
2020-04-18 20:13:05 +0300

    Урок №52

---
+++ src/Rectangle4.java
@@ -0,0 +1,28 @@
+//Урок №52
+public class Rectangle4 {
+    double width;
+    double height;
+
+    double square() {
+        return width * height;
+    }
+
+    void setValues(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    public static void main(String[] args) {
+
+        Rectangle4 rect, rect2, rect3;
+
+        rect = new Rectangle4();
+        rect2 = new Rectangle4();
+        rect3 = rect;
+
+        rect.setValues(10, 2);
+        rect2.setValues(5, 3);
+        System.out.println("Square 1 = " + rect.square());
+        System.out.println("Square 2 = " + rect2.square());
+    }
+}
