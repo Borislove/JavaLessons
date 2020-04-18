60fb8136464cfccf1947fbee37d6eb02fb39cdff
Boris <abirme@yandex.ru>
2020-04-18 20:08:08 +0300

    Урок №51. Java. Методы класса

---
+++ src/Rectangle3.java
@@ -0,0 +1,29 @@
+//Урок №51. Java. Методы класса
+public class Rectangle3 {
+    double width;
+    double height;
+
+    double square() {
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+
+        Rectangle3 rect, rect2, rect3;
+
+        rect = new Rectangle3();
+        rect2 = new Rectangle3();
+        rect3 = rect;
+
+        rect.width = 10;
+        rect.height = 2;
+
+        rect3.width = 4;
+        rect3.height = 4;
+
+        rect2.width = 5;
+        rect2.height = 3;
+
+        System.out.println("Square = " + rect.square());
+    }
+}
