b5e4a2155945ddb44a10d91571f96363235a6088
Boris <abirme@yandex.ru>
2020-04-18 22:03:15 +0300

    Урок №60. Java. Статические элементы

---
+++ src/MyStatic.java
@@ -0,0 +1,15 @@
+//Урок №60. Java. Статические элементы
+public class MyStatic {
+
+    static int a = 3;
+
+    static void out() {
+        System.out.println("a = " + a);
+    }
+}
+
+class StatTest {
+    public static void main(String[] args) {
+        MyStatic.out();
+    }
+}
---
+++ src/Rect.java
@@ -0,0 +1,15 @@
+//Урок №59. Java. Работа с несколькими классами
+public class Rect {
+    int width;
+    int height;
+}
+
+class RectTest {
+    public static void main(String[] args) {
+
+        Rect rec = new Rect();
+        rec.width = 5;
+        rec.height = 2;
+
+    }
+}
