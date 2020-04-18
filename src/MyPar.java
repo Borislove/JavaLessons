114e5f8b7d5ab3c42dbdb9158d575515dc281de7
Boris <abirme@yandex.ru>
2020-04-18 21:01:48 +0300

    Урок №56. Объекты в качестве аргументов и параметров

---
+++ src/MyPar.java
@@ -0,0 +1,24 @@
+//Урок №56. Объекты в качестве аргументов и параметров
+public class MyPar {
+    int a;
+    int b;
+
+    MyPar(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    boolean eq(MyPar myPar) {
+        if (a == myPar.a & b == myPar.b)
+            return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        MyPar o1, o2;
+        o1 = new MyPar(1, 2);
+        o2 = new MyPar(1, 2);
+
+        System.out.println(o1.eq(o2));
+    }
+}
