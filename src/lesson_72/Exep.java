9415149f245c2fa1a7427ed66519fab2f6115927
Boris <abirme@yandex.ru>
2020-04-23 23:07:38 +0300

    Урок №72. Обработка исключений

---
+++ src/lesson_72/Exep.java
@@ -0,0 +1,18 @@
+package lesson_72;
+
+//Урок №72. Обработка исключений
+
+public class Exep {
+
+    static void do_it() {
+        int a = 80;
+        int b = 0;
+
+        int c = a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Exep.do_it();
+    }
+}
