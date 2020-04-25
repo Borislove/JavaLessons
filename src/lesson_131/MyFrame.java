d07f789d82c111b753b8525b65e645e5effb1ed6
Boris <abirme@yandex.ru>
2020-04-25 14:52:57 +0300

    Урок №132.Рисование в JFrame

--- src/lesson_131/MyFrame.java
+++ src/lesson_131/MyFrame.java
@@ -1,11 +1,19 @@
 package lesson_131;
 
 import javax.swing.*;
+import java.awt.*;
 
-//Урок №131. Java. Работа с JFrame
+//Урок №132.Рисование в JFrame
 public class MyFrame extends JFrame {
 
+    public void paint(Graphics g) {
+        g.setFont(new Font("Serif", Font.ITALIC, 30));
+        g.drawString("Hello Java!", 20, 100);   //текст, расположение
+    }
+
     public MyFrame(String str) {
+
+
         super(str);
         setSize(400, 150);
         setVisible(true);
