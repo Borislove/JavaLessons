d4e44cd67105e36044894a3f7142278c9d10b840
Borislove <abirme@yandex.ru>
2020-05-07 11:14:34 +0300

    Урок №136. Кнопки

---
+++ src/lesson_136/MyFrame.java
@@ -0,0 +1,49 @@
+package lesson_136;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//Урок №136. Кнопки
+public class MyFrame extends JFrame {
+
+    JButton button;
+
+    public MyFrame(String str) {
+        super(str);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        JLabel label = new JLabel("My Label");  //метка
+        label.setToolTipText("Label text");   //текст подсказка
+        label.setText("Text");
+        c.add(label);
+
+        JLabel label2 = new JLabel("My Labe2");  //метка
+        c.add(label2);
+
+        button = new JButton("My button");
+        c.add(button);
+        Handler handler = new Handler();
+        button.addActionListener(handler);
+
+        setSize(400, 150);
+
+        setVisible(true);
+
+    }
+
+    class Handler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == button)
+                JOptionPane.showMessageDialog(null, "button");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame("My frame");
+    }
+}
+
+
+
