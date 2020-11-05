a39058c8ffefd898966cec980f7d96f9bbc5262a
Borislove <abirme@yandex.ru>
2020-11-05 09:55:11 +0300

    Урок №30 , move package (инкремент, декремент)

---
+++ src/lesson_30/Decrement.java
@@ -0,0 +1,12 @@
+package lesson_30;
+
+public class Decrement {
+    public static void main(String[] args) {
+
+        int a = 10;
+        a --;
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+    }
+}
---
+++ src/lesson_30/Increment.java
@@ -0,0 +1,11 @@
+package lesson_30;
+
+public class Increment {
+    public static void main(String[] args) {
+
+        int a = 2;
+        a++; //постфиксный
+        System.out.println(a);
+        ++a; //префиксный
+    }
+}
