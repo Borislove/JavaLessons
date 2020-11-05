2f508fc08cb6d55fda30ea38e491b2141359a5d1
Borislove <abirme@yandex.ru>
2020-11-06 01:34:24 +0300

    Урок №36 , move package (while, do-while)

--- src/lesson_36/MyDoWhile.java
+++ src/lesson_36/MyDoWhile.java
@@ -1,14 +1,18 @@
-//Урок №36. Оператор цикла while, do-while
-public class MyWhile {
+package lesson_36;
+
+public class MyDoWhile {
 
     public static void main(String[] args) {
 
         int i = 0;
 
-        while (i < 10) {
+        do {
             System.out.println(i);
             i++;
         }
+        while (i < 10);
         System.out.println("Finish");
     }
 }
+
+
--- src/lesson_36/MyWhile.java
+++ src/lesson_36/MyWhile.java
@@ -1,16 +1,16 @@
-public class MyDoWhile {
+package lesson_36;
+
+//Урок №36. Оператор цикла while, do-while
+public class MyWhile {
 
     public static void main(String[] args) {
 
         int i = 0;
 
-        do {
+        while (i < 10) {
             System.out.println(i);
             i++;
         }
-        while (i < 10);
         System.out.println("Finish");
     }
 }
-
-
