2841b0a3d61d5dfada39561a780e0ad31500dbab
Borislove <abirme@yandex.ru>
2020-11-06 03:41:13 +0300

    :octocat: Урок №41 , move package (array)

---
+++ src/lesson_41/MyArr.java
@@ -0,0 +1,19 @@
+package lesson_41;
+
+public class MyArr {
+
+    public static void main(String[] args) {
+
+        int point[] = {4, 17, 13};
+
+        int point2[] = new int[3]; //резервируем память
+        //инициализация
+        point2[0] = 4;
+        point2[1] = 17;
+        point2[2] = 13;
+
+        int point3[] = new int[]{4, 17, 13};
+
+        int myArray[] = {1,2,3,4,5};
+    }
+}
