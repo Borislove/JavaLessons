d4f433ab24330c7f6948b80d4671330bab61ca90
Borislove <abirme@yandex.ru>
2020-11-06 03:20:01 +0300

    :octocat: Урок №40 , move package (continue)

--- src/lesson_40/MyCon.java
+++ src/lesson_40/MyCon.java
@@ -1,9 +1,11 @@
+package lesson_40;
+
 //Урок №40. Операторы перехода continue, return
 public class MyCon {
     public static void main(String[] args) {
         first:
         for (int i = -5; i < 5; i++) {
-            //    if (i == 5) continue;
+            //    if (i == 5) continue; переход на следующую итерацию
             if (i == 0) continue first;
             System.out.println("i = " + 1 / i);
         }
