36e0a58cae37e73d68d2aa41e8494479251b2b44
Borislove <abirme@yandex.ru>
2020-11-07 08:58:26 +0300

    :octocat: Урок №59 Работа с несколькими классами

---
+++ src/lesson_59/Rect.java
@@ -0,0 +1,17 @@
+package lesson_59;
+
+//Урок №59. Работа с несколькими классами
+public class Rect {
+    int widht;
+    int height;
+}
+
+class RectTest {
+
+    public static void main(String[] args) {
+
+        Rect rec = new Rect();
+        rec.widht = 5;
+        rec.height = 2;
+    }
+}
