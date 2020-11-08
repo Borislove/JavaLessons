df3c2a64fad8a2109b1b50ef1dded44327695e59
Borislove <abirme@yandex.ru>
2020-11-08 08:43:40 +0300

    :octocat: Урок №77 Ключевое слово throws

---
+++ src/lesson_77/MyThrows.java
@@ -0,0 +1,19 @@
+package lesson_77;
+
+//Урок №77. Ключевое слово throws
+public class MyThrows {
+
+    static void one() throws IllegalAccessException {
+
+        throw new IllegalAccessException("Test");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            one();
+        } catch (IllegalAccessException e) {
+            System.out.println("" + e);
+        }
+    }
+}
