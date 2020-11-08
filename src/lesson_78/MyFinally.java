52685019b683196c5ba315e173025a2c1456dd9d
Borislove <abirme@yandex.ru>
2020-11-08 09:50:33 +0300

    :octocat: Урок №78 Блок finally

---
+++ src/lesson_78/MyFinally.java
@@ -0,0 +1,47 @@
+package lesson_78;
+
+//Блок finally
+public class MyFinally {
+
+    static void firstMethod() {
+
+        try {
+            System.out.println("In First method");
+            throw new RuntimeException("Test");
+        } finally {
+            System.out.println("finally for First method");
+        }
+    }
+
+    static void secondMethod() {
+
+        try{
+            System.out.println("In Second method");
+            return;
+        }finally {
+            System.out.println("finally for Second method");
+        }
+    }
+
+    static void thirdMethod(){
+
+        try{
+            System.out.println("In Third method");
+        }finally {
+            System.out.println("finally for Third method");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            firstMethod();
+        }catch (Exception e){
+            System.out.println("Exception");
+        }
+
+        secondMethod();
+        thirdMethod();
+    }
+}
+
