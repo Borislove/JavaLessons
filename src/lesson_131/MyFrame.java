529be27605fe83e892555fc98a28f45f91b0ac92
Borislove <abirme@yandex.ru>
2020-11-13 09:54:20 +0300

    :octocat: Урок №131. Java. Работа с JFrame

--- src/lesson_131/MyFrame.java
+++ src/lesson_131/MyFrame.java
@@ -3,13 +3,9 @@ package lesson_131;
 import javax.swing.*;
 import java.awt.*;
 
-//Урок №132.Рисование в JFrame
+//Урок №131. Работа с JFrame
+//вывод простого окна
 public class MyFrame extends JFrame {
-
-    public void paint(Graphics g) {
-        g.setFont(new Font("Serif", Font.ITALIC, 30));
-        g.drawString("Hello Java!", 20, 100);   //текст, расположение
-    }
 
     public MyFrame(String str) {
         super(str);
