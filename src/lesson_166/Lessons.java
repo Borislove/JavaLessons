fb43c6255e037e125d206cca55c2388c07387487
Borislove <abirme@yandex.ru>
2020-12-26 02:32:06 +0300

    :octocat: Урок №166 Создание класса и работа с ним в классе ExerciseGUI

---
+++ src/lesson_166/Lessons.java
@@ -0,0 +1,30 @@
+package lesson_166;
+
+public class Lessons {
+
+    private String title;
+    private String instructions;
+    private String text;
+
+    public Lessons(String title, String instrucions, String text) {
+        this.title = title;
+        this.instructions = instructions;
+        this.text = text;
+    }
+
+    public String get_title() {
+        if (title != null) return title;
+        else return "Title";
+    }
+
+    public String get_instructions() {
+        if (instructions != null) return instructions;
+        else return "Instructions";
+    }
+
+    public String get_text() {
+        if (text != null) return text;
+        else return "Text";
+    }
+}
+
